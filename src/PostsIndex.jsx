d606f03ae226dee20093c5161fb47d7802b521e3
jordanoboyle <jmcmcode@gmail.com>
2024-05-28 13:42:40 -0600

    Added key={post.id}, eliminates child not having key reference

--- src/PostsIndex.jsx
+++ src/PostsIndex.jsx
@@ -1,17 +1,14 @@
 export function PostsIndex(props) {
-  console.log(props)
   return (
     <div id="posts-index">
       <h1>ALL POSTS:</h1>
       <p>pre loop text just for reference</p>
       {props.posts.map(post => (
-        <div className="posts"> 
+        <div key={post.id} className="posts"> 
           <h2>{post.title}</h2>
           <p>{post.date}</p>
           <img src={post.image}/>
-          <p>
-           Post: {post.body}
-          </p>
+          <p>Post: {post.body}</p>
         </div>
       ))};
     </div>
